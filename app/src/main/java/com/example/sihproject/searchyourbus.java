c7621ddfc19e6a14fd28ba39917e4c55da6e1748
astro <astro.sumit.usa@gmail.com>
2023-11-04 17:54:21 +0530

    my first app

---
+++ app/src/main/java/com/example/sihproject/searchyourbus.java
@@ -0,0 +1,14 @@
+package com.example.sihproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class searchyourbus extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_searchyourbus);
+    }
+}
